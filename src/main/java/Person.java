f99af821d7399750a69c397e0154dab0b8884244
prosacwroc <prosac.wroc@gmail.com>
2018-06-29 18:31:36 +0200

    Janusz

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,7 +1,22 @@
 public class Person {
-    String name;
-    String surname;
-    int age;
-    int height;
-    String hobby;
+    public String name;
+    public String surname;
+    public int age;
+    public int height;
+    public String hobby;
+
+    Person Janusz;
+
+    {
+        Janusz = new Person();
+        Janusz.name = "Janusz";
+        Janusz.surname = "Grażyna";
+        Janusz.age = 44;
+        Janusz.height = 167;
+        Janusz.hobby = "Somsiad";
+    }
 }
+
+
+
+
